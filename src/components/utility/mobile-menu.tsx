1532fb79459e5c7dbc8711590bb1739191e92358
webcrafter000 <nagendranalam1611@gmail.com>
2026-01-04 11:55:31 +0530

    updating portfolio

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -58,7 +58,7 @@ export default function MobileMenu({
                 ))}
                 <ThemeSwitch setClose={setOpenMenu} />
               </div>
-              <div className="absolute bottom-0 py-6">©2025 Nalam Nagendra</div>
+              <div className="absolute bottom-0 py-6">©2026 Nalam Nagendra</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
